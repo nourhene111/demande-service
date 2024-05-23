e6fa91dee8dc272def100877140d0c561716b6cc
nourhene111 <nourhenesaidi22@gmail.com>
2024-05-23 10:49:36 +0100

    Initial commit

---
+++ models/index.js
@@ -0,0 +1,9 @@
+
+const { Demande, DemandeSchema } = require('./demande.model');
+
+function setupModels(sequelize) {
+    Demande.init(DemandeSchema, Demande.config(sequelize));
+}
+
+module.exports = setupModels;
+
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+
+const express = require('express'); 
+
+const demandeRouter = require('./demande.route');
+
+function routerApi(app) {
+  const router = express.Router();
+  app.use('/api', router); 
+  router.use('/demande', demandeRouter);
+}
+
+module.exports = routerApi;
